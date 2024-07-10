f116eb0f88dd4b66da917a7e23f924931c6162eb
JawadBLK <100697289+JawadBLK@users.noreply.github.com>
2024-07-10 10:36:51 +0200

    Create zombie.java

---
+++ CODE/Exo5/zombie.java
@@ -0,0 +1,29 @@
+public class Zombie {
+    private int x;
+    private int y;
+
+    public Zombie(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void deplacerAleatoirement() {
+        Random random = new Random();
+        switch (random.nextInt(4)) {
+            case 0:
+                y--;
+                break;
+            case 1:
+                y++;
+                break;
+            case 2:
+                x--;
+                break;
+            case 3:
+                x++;
+                break;
+        }
+    }
+
+    // Getters et Setters
+}
