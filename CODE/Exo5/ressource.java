f8e147183f7889595dbd254fca1392e5f0f5b5ae
JawadBLK <100697289+JawadBLK@users.noreply.github.com>
2024-07-10 10:25:53 +0200

    Create ressource.java

---
+++ CODE/Exo5/ressource.java
@@ -0,0 +1,13 @@
+public class Ressource {
+    private final String type;
+    private final int x;
+    private final int y;
+
+    public Ressource(String type, int x, int y) {
+        this.type = type;
+        this.x = x;
+        this.y = y;
+    }
+
+    // Getters et Setters
+}
