4093426620339da58821101a593637051638eab1
JawadBLK <100697289+JawadBLK@users.noreply.github.com>
2024-07-10 10:25:20 +0200

    Create carte.java

---
+++ CODE/Exo5/carte.java
@@ -0,0 +1,21 @@
+public class Carte {
+    private final int taille;
+    private final List<Ressource> ressources;
+    private final List<Zombie> zombies;
+
+    public Carte(int taille) {
+        this.taille = taille;
+        this.ressources = new ArrayList<>();
+        this.zombies = new ArrayList<>();
+    }
+
+    public void ajouterRessource(Ressource ressource) {
+        ressources.add(ressource);
+    }
+
+    public void ajouterZombie(Zombie zombie) {
+        zombies.add(zombie);
+    }
+  
+    // Getters et Setters
+}
