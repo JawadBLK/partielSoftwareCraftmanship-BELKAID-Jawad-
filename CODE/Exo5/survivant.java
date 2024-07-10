8c8b0ebfd0dc84a58cf7654e9e49f3a0c4455438
JawadBLK <100697289+JawadBLK@users.noreply.github.com>
2024-07-10 10:30:52 +0200

    Update survivant.java

--- CODE/Exo5/survivant.java
+++ CODE/Exo5/survivant.java
@@ -32,4 +32,43 @@ public class Survivant {
         sante -= points;
     }
 
-}
+    public void collecterRessource(Ressource ressource) {
+        inventaire.add(ressource);
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public String getOrientation() {
+        return orientation;
+    }
+
+    public void setOrientation(String orientation) {
+        this.orientation = orientation;
+    }
+
+    public int getSante() {
+        return sante;
+    }
+
+    public void setSante(int sante) {
+        this.sante = sante;
+    }
+
+    public List<Ressource> getInventaire() {
+        return inventaire;
+    }
+} 
